6fb0b3bd578edbe3041f97a8fcd37a996d31e12a
Tony Rieker <tony@plenary.org>
2015-11-14 21:58:32 -0800

    first pass with SD card lib

--- app.cpp
+++ app.cpp
@@ -1,146 +1,125 @@
+/*
+  SD card test
+
+ This example shows how use the utility libraries on which the'
+ SD library is based in order to get info about your SD card.
+ Very useful for testing a card when you're not sure whether its working or not.
+
+ The circuit:
+  * SD card attached to SPI bus as follows:
+  Refer to "libraries/SdFat/Sd2Card_config.h"
+
+ created  28 Mar 2011
+ by Limor Fried
+ modified 16 Mar 2011
+ by Tom Igoe
+ modified for Maple(STM32 micros)/libmaple
+ 17 Mar 2012
+ by dinau
+ */
+ // include the SD library:
+
 #include "application.h"
-#include "SparkFun_Photon_Weather_Shield_Library.h"
+#include "SD.h"
 
-float humidity = 0;
-float tempf = 0;
-float pascals = 0;
-float inhg = 0;
-float baroTemp = 0;
-float altf = 0;
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
 
-int iterations = 0;
+// SOFTWARE SPI pin configuration - modify as required
+// The default pins are the same as HARDWARE SPI
+const uint8_t chipSelect = A2;    // Also used for HARDWARE SPI setup
+const uint8_t mosiPin = A5;
+const uint8_t misoPin = A4;
+const uint8_t clockPin = A3;
 
-char publishString[64];
+int ledOn = false;
 
-//Create Instance of HTU21D or SI7021 temp and humidity sensor and MPL3115A2 barrometric sensor
-Weather sensor;
-
-void initWeather() {
-  //Initialize the I2C sensors and ping them
-  sensor.begin();
-
-  sensor.setModeBarometer();
-
-  sensor.setOversampleRate(7);
-
-  sensor.enableEventFlags();
-}
-
-void endWeather()
-{
-  sensor.end();
-}
-
-float readBaro() {
-  sensor.setModeBarometer();
-  return sensor.readBaroTempF();
-}
-
-float readAltitude() {
-  sensor.setModeAltimeter();
-  return sensor.readAltitudeFt();
-}
-
-// Measure Pressure from the MPL3115A2
-// When device first boots it may take a hundred or so
-// iterations before the value accurate
-float readPressure() {
-  float pa = sensor.readPressure();
-  // try again value is incorrect
-  if (pa < 10.00) {
-    return readPressure();
-  }
-  return pa;
-}
-
-void getWeather()
-{
-  // Measure Relative Humidity from the HTU21D or Si7021
-  humidity = sensor.getRH();
-
-  // Measure Temperature from the HTU21D or Si7021
-  tempf = sensor.getTempF();
-  // Temperature is measured every time RH is requested.
-  // It is faster, therefore, to read it from previous RH
-  // measurement with getTemp() instead with readTemp()
-  pascals = readPressure();
-
-  //Measure the Barometer temperature in F from the MPL3115A2
-  //baroTemp = readBaro();
-
-  //If in altitude mode, you can get a reading in feet  with this line:
-  //altf = readAltitude();
-}
-
-int getAndPublishWeather(String command)
-{
-  iterations++;
-
-  initWeather();
-
-  Serial1.print("getAndPublishWeather() iteration: ");
-  Serial1.println(iterations);
-
-  // turn on D7 led while taking measurements
-  digitalWrite(D7, HIGH);
-
-  getWeather();
-
-  // TODO: still missing sprintf floats
-  //sprintf(publishString, "{\"temp\": %.2f, \"hum\": %.2f, \"pa\": %.2f}", tempf, humidity, pascals);
-  sprintf(publishString, "{\"temp\": %d, \"hum\": %d, \"pa\": %d}", (int)(tempf*100), (int)(humidity*100), (int)(pascals*100));
-
-  Serial1.println(publishString);
-  Serial1.print("tempf:"); Serial1.println(tempf);
-  Serial1.print("hum:"); Serial1.println(humidity);
-  Serial1.print("pa:"); Serial1.println(pascals);
-
-  if (Particle.connected()) {
-    Particle.publish("weather", publishString);
-  } else {
-    Serial1.println("Unable to publish weather, no cloud connection.");
-  }
-
-  endWeather();
-
-  digitalWrite(D7, LOW);
-
-  return 1;
-}
-
-void saveWeatherData()
-{
-  Serial1.println("saveWeatherData()");
-}
-
-//---------------------------------------------------------------
 void setup()
 {
-  Serial1.begin(9600);
-
-  Serial1.println("setup()");
 
   pinMode(D7, OUTPUT);
 
-  Particle.function("getWeather", getAndPublishWeather);
-}
+  Serial1.begin(9600);
+  Serial1.println("Serial test");
 
-//---------------------------------------------------------------
-void loop()
-{
-  const unsigned long sampleInterval = 1 * 60 * 1000UL;
-  static unsigned long lastSampleTime = 0 - sampleInterval;  // initialize such that a reading is due the first time through loop()
+  Serial1.print("\nInitializing SD card...");
 
-  unsigned long now = millis();
-  if (now - lastSampleTime >= sampleInterval)
-  {
-    lastSampleTime += sampleInterval;
-    // add code to take temperature reading here
-    getAndPublishWeather("");
-    saveWeatherData();
+  // we'll use the initialization code from the utility libraries
+  // since we're just testing if the card is working!
+  // Initialize HARDWARE SPI with user defined chipSelect
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+
+  // Initialize SOFTWARE SPI (uncomment and comment out above line to use)
+  //  if (!card.init(mosiPin, misoPin, clockPin, chipSelect)) {
+    Serial1.println("initialization failed. Things to check:");
+    Serial1.println("* is a card is inserted?");
+    Serial1.println("* Is your wiring correct?");
+    Serial1.println("* did you change the chipSelect pin to match your shield or module?");
+    return;
+  } else {
+   Serial1.println("Wiring is correct and a card is present.");
   }
 
-  System.sleep(SLEEP_MODE_DEEP);
+  // print the type of card
+  Serial1.print("\nCard type: ");
+  switch(card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial1.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial1.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial1.println("SDHC");
+      break;
+    default:
+      Serial1.println("Unknown");
+  }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial1.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    return;
+  }
+
+  // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial1.print("\nVolume type is FAT");
+  Serial1.println(volume.fatType(), DEC);
+  Serial1.println();
+
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize *= 512;                         // SD card blocks are always 512 bytes
+  Serial1.print("Volume size (bytes): ");
+  Serial1.println(volumesize);
+  Serial1.print("Volume size (Kbytes): ");
+  volumesize /= 1024;
+  Serial1.println(volumesize);
+  Serial1.print("Volume size (Mbytes): ");
+  volumesize /= 1024;
+  Serial1.println(volumesize);
+  Serial1.print("Volume size (Gbytes): ");
+  volumesize /= 1024;
+  Serial1.println(volumesize);
+
+  Serial1.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+
+  card.end();
 }
 
-
+void loop(void) {
+  if (millis() % 1000 == 0 && ledOn) {
+      digitalWrite(D7, LOW);
+      ledOn = !ledOn;
+  } else if (millis() % 1000 == 0) {
+      digitalWrite(D7, HIGH);
+      ledOn = !ledOn;
+  }
+}
